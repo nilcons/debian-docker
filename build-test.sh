8e0f76dfefc650878173ce8844a586251f22b9f2
Gergely Risko <errge@nilcons.com>
2026-09-11 18:25:48 +0200

    Use zstd layer compression for better end-user experience

--- build-test.sh
+++ build-test.sh
@@ -2,5 +2,6 @@
 
 set -euo pipefail
 
-docker build -t ttl.sh/nilcons/debian:2h .
-docker push ttl.sh/nilcons/debian:2h
+docker buildx create --name nilcons-debian-zstd-builder --driver docker-container
+trap 'docker buildx rm nilcons-debian-zstd-builder' EXIT
+docker buildx build --builder nilcons-debian-zstd-builder --provenance=false --output type=image,name=ttl.sh/nilcons/debian:2h,compression=zstd,force-compression=true,oci-mediatypes=true,push=true .
